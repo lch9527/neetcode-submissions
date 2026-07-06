afba028d61bb6c1a63aa30bf8f89e0075c1fef1f
lch9527 <38733164+lch9527@users.noreply.github.com>
2026-07-06 16:51:58 -0400

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> nset;
+
+        for(const int i: nums){
+            if(nset.find(i) == nset.end()){
+                nset.insert(i);
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+};

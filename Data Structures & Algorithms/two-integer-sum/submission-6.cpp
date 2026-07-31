d20c5de42eee2f34d09b03cff9a59bc280fe5c78
lch9527 <38733164+lch9527@users.noreply.github.com>
2026-07-31 13:23:36 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> nmap;
+        // key = num , val = index;
+
+        for(int i = 0 ; i< nums.size(); i++){
+            int find = target - nums[i];
+            if(nmap.find(find) != nmap.end()){
+                return {nmap[find], i};
+            }
+            else{
+                nmap[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
